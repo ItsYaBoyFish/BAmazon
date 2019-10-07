e16c3d092150f893b7f64691085e9e52ae8432e3
Austin Graves <austingraves106@aol.com>
2019-10-07 16:15:00 -0500

    I changed the file name to the appropriate one per the homework instructions. As well as tested some theories about display information to a user.

---
+++ bamazonCustomer.js
@@ -0,0 +1,18 @@
+const inquirer = require('inquirer');
+
+// inquirer.prompt(
+//   [
+//     {
+
+//     }
+//   ]
+// );
+
+// var data = [
+//   {Name: 'Austin', Gender: 'Male', Age: 23},
+//   {Name: 'Tina', Gender: 'Female', Age: 31},
+//   {Name: 'Alison', Gender: 'Female', Age: 30},
+//   {Name: 'Erica', Gender: 'Female', Age: 29}  
+// ];
+
+// console.table(data);

e550961136b159858f6affb9cac12da6e3cbd18f
Austin Graves <austingraves106@aol.com>
2019-10-07 17:04:41 -0500

    I finished up the required portion of the homework. I have the database connection setup. All of the tables functioning. As well as all of the logic to control when a quantity ordered is above the stock level inside the database.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,18 +1,70 @@
+require('dotenv').config();
+const keys = require('./keys.js');
 const inquirer = require('inquirer');
+const mysql = require('mysql');
 
-// inquirer.prompt(
-//   [
-//     {
+// Database Setup ============================================
 
-//     }
-//   ]
-// );
+var db = mysql.createConnection({
+  host: keys.host,
+  user: keys.user,
+  password: keys.password,
+  database: keys.database
+});
 
-// var data = [
-//   {Name: 'Austin', Gender: 'Male', Age: 23},
-//   {Name: 'Tina', Gender: 'Female', Age: 31},
-//   {Name: 'Alison', Gender: 'Female', Age: 30},
-//   {Name: 'Erica', Gender: 'Female', Age: 29}  
-// ];
+db.connect(function(err) {
+  if (err) {
+    console.log(err);
+  }
+});
 
-// console.table(data);
+db.query("SELECT * FROM products;", function(err, results) {
+  if (err) {
+    console.log(err);
+  } else {
+    var totalPrice;
+    console.table(results);
+    inquirer.prompt(
+      [
+        {
+          type: 'text',
+          message: 'What is the item_id of the item you would like to purchase?',
+          name: 'idSelected'
+        },
+        {
+          type: 'text',
+          message: 'How many would you like to purchase?',
+          name: 'quantityWantingToPurchase'
+        }
+      ]
+    ).then(function(userInput) {
+      // console.log(userInput);
+      var id = parseInt(userInput.idSelected);
+      var itemQty = parseInt(userInput.quantityWantingToPurchase)
+      var dbStockLevel = results[id - 1].stock_quantity;
+      totalPrice = results[id - 1].price * itemQty;
+      // console.log(`ID: ${id}`);
+      // console.log(`itemQty: ${itemQty}`);
+      // console.log(`dbStockLevel: ${dbStockLevel}`);
+      evaluateStockingQuantities(dbStockLevel,itemQty,id, totalPrice);
+    });
+  }
+});
+
+
+
+function evaluateStockingQuantities(yourDatabaseStockLevel, AmountWantedToPurchase, itemAttemptingToPurchaseID, totalPriceOfPurchase) {
+  var itemsSold;
+  if (AmountWantedToPurchase > yourDatabaseStockLevel) {
+    console.log('Insufficient Quantity!');
+    itemsSold = false;
+  } else {
+    db.query(`UPDATE products SET stock_quantity=${yourDatabaseStockLevel-AmountWantedToPurchase} WHERE item_id=${itemAttemptingToPurchaseID}`)
+    itemsSold = true;
+  } 
+  if (itemsSold === true) {
+    console.log(`Thanks For Shopping With Us! Your Total Is: $${totalPriceOfPurchase}`);
+  } else {
+    console.log('Sorry For The Inconvenience, Please Check Back In A Couple Of Days!');
+  }
+};

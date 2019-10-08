efb8a6d1b0808e6d693e25c203831d56dbcb6b8b
Austin Graves <austingraves106@aol.com>
2019-10-08 18:32:56 -0500

    Added the additional column needed for product_sales and beginning stages of phase 3

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -36,7 +36,9 @@ function evaluateStockingQuantities(yourDatabaseStockLevel, AmountWantedToPurcha
     console.log('Insufficient Quantity!');
     itemsSold = false;
   } else {
-    db.query(`UPDATE products SET stock_quantity=${yourDatabaseStockLevel-AmountWantedToPurchase} WHERE item_id=${itemAttemptingToPurchaseID}`)
+    db.query(`UPDATE products SET stock_quantity=${yourDatabaseStockLevel-AmountWantedToPurchase} WHERE item_id=${itemAttemptingToPurchaseID}`);
+    // Updating the product_cost column here.
+    db.query(`UPDATE products SET product_sales=${totalPriceOfPurchase} WHERE item_id=${itemAttemptingToPurchaseID}`);
     itemsSold = true;
   } 
   if (itemsSold === true) {
@@ -61,19 +63,22 @@ function userPrompts(resultsFromTheDBQuery) {
       }
     ]
   ).then(function(userInput) {
-    if (userInput.idSelected > resultsFromTheDBQuery.length) {
+    // if (userInput.idSelected > resultsFromTheDBQuery.length) {
+      if (1 === 2) {
       console.log('Please Enter A Valid item_id!');
       userPrompts(resultsFromTheDBQuery);
     } else {
     // console.log(userInput);
     var id = parseInt(userInput.idSelected);
     var itemQty = parseInt(userInput.quantityWantingToPurchase)
+    // Used to have a minus 1 for the id here. and total price.
     var dbStockLevel = resultsFromTheDBQuery[id - 1].stock_quantity;
     totalPrice = resultsFromTheDBQuery[id - 1].price * itemQty;
     // console.log(`ID: ${id}`);
     // console.log(`itemQty: ${itemQty}`);
     // console.log(`dbStockLevel: ${dbStockLevel}`);
     evaluateStockingQuantities(dbStockLevel,itemQty,id, totalPrice);
+    userPrompts();
     } 
   });
 };
